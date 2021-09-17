e32ddd8079a1087a14ce06c003cd67e7eeb39357
mandagouveia <amanda_gouveia_@hotmail.com>
2021-09-17 19:43:40 -0300

    Novo commit do Java treino

---
+++ Treinando_Java/src/Projeto_java_inicial.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Projeto_java_inicial {
+
+	public static void main(String[] args) {
+		// Variáveis;
+		double sl1=0, sl2= 0;
+		
+		// entrada de dados
+				Scanner s = new Scanner(System.in);
+				System.out.println("Quanto você está ganhando agora?");
+				sl1 = s.nextDouble();
+				sl2 = sl1*1.15;
+				
+				
+		// processamento
+		
+				// saída
+				System.out.println("Então você passará a ganhar: " +sl2);
+				
+			}
+				
+				
+		
+
+	}
+
